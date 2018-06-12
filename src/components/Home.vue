95c182bf3db2b2440ae3b5c9bd6ef35b727424ee
Dmitrii Savchenko <dsavax@gmail.com>
2018-06-12 03:05:06 +0300

    Supporting content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,48 @@
           </div>
         </div>
       </section>
+
+      <section class="section">
+        <div class="container">
+          <div class="columns pd is-desktop">
+            <div class="column is-1 has-text-centred">
+              <i class="fa fa-cog is-primary"></i>
+            </div>
+            <div class="column is-one-third-desktop">
+              <p class="title"><strong>We provide superior logistis so that your business can succeed in a crazy world.</strong></p>
+            </div>
+            <div class="column">
+              <p>Lorem isum dolor sit amet, и немного рыбы на русском жфывдлаофжывлдрпшгукрщшпгLorem isum dolor sit amet.</p>
+            </div>
+          </div>
+        </div>
+        <div class="columns pd">
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+                <p class="subtitle">Gary Simon</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+                <p class="subtitle">Gary Simon</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this thing.</p>
+                <p class="subtitle">Gary Simon</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
   </div>
 </template>
 
@@ -45,5 +87,17 @@ export default {
 
   h2
     margin: 1.5rem 0 2rem 0 !important
+
+  .fa-cog
+    font-size: 40px
+
+  #learn
+    +desktop
+      margin-bottom: 2rem
+
+  .pd
+    +tablet
+      padding: 2em 0
+
 
 </style>
